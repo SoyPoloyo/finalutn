c34ec1010dc2b78741792ee5341a22e44f7b4763
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-01-29 22:03:03 -0600

    Update game-lista.component.ts

--- cliente/src/app/componentes/game-lista/game-lista.component.ts
+++ cliente/src/app/componentes/game-lista/game-lista.component.ts
@@ -32,7 +32,7 @@ export class GameListaComponent implements OnInit {
             res => {
 
               this.games = res;
-              this.gamesid = this.games.id;
+              this.gamesid = res[0].id
 
             },
             err => console.error(err)
@@ -50,11 +50,28 @@ export class GameListaComponent implements OnInit {
   }
 
   
-    redireccionPersonajes(){
+    redireccionPersonajes(tomarJuegos){
 
   
             localStorage.setItem("card_id", this.games.id);
 
+            console.log(this.gamesid);
+
+            this.juegosServicio.capturaGame(localStorage.id).subscribe(
+
+              res => {
+                
+                let i=0;
+               
+                this.gamesid = res[i].id
+                console.log(this.games[1].id, "hola");
+  
+              },
+              err => console.error(err)
+            );
+
+
+    
       }
  
 
